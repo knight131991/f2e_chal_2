153d1fcd34d68f0fab0685281d85bde7a35701ba
Jack.Lin <knight131991@yahoo.com.tw>
2022-05-19 19:20:32 +0800

    implement CheckboxGroup

---
+++ src/component/CheckboxGroup.jsx
@@ -0,0 +1,28 @@
+import { Checkbox } from "antd";
+import styled from "styled-components";
+import styleParams from "../constant/styleParams";
+
+export default styled(Checkbox.Group)`
+  & .ant-checkbox-inner {
+    width: 20px;
+    height: 20px;
+  }
+  & .ant-checkbox-checked .ant-checkbox-inner {
+    background-color: ${styleParams.mainColorDark};
+    &:after {
+      transform: rotate(45deg) scale(1.2) translate(-50%, -50%);
+    }
+  }
+  & .ant-checkbox-inner:after {
+    left: 4.7px;
+  }
+  & .ant-checkbox-wrapper {
+    font-size: 16px;
+    white-space: nowrap;
+  }
+  .ant-checkbox-input:focus + .ant-checkbox-inner,
+  .ant-checkbox-wrapper:hover .ant-checkbox-inner,
+  .ant-checkbox:hover .ant-checkbox-inner {
+    border-color: ${styleParams.mainColorDark};
+  }
+`;
