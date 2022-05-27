b78da30f5f6e338dd6cf51453cf4396bb609173b
Jack.Lin <knight131991@yahoo.com.tw>
2022-05-28 02:22:44 +0800

    implement ToTopBtn

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 // import PropTypes from "prop-types";
 import Footer from "../../component/Footer";
 import Button from "../../component/Button";
@@ -13,6 +13,11 @@ import useRWD, { useRWDStyleParams } from "../../hooks/useRWD";
 import PageContainer from "../../component/PageContainer";
 import screenEnum from "../../constant/screenEnum";
 import styleParams from "../../constant/styleParams";
+import { ReactComponent as ToTop } from "../../images/icon/ToTop.svg";
+
+const ScrolledContainer = styled.div`
+  overflow: auto;
+`;
 
 const Container = styled(PageContainer)`
   width: 100%;
@@ -59,42 +64,82 @@ const Title = styled.span`
   font-size: ${({ isSmScreen }) => (isSmScreen ? "32px" : "40px")};
 `;
 
+const ToTopBtn = styled(({ show, right, ...rest }) => <div {...rest} />)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  cursor: pointer;
+  bottom: 70px;
+  right: ${({ right }) => right}px;
+  width: 70px;
+  height: 70px;
+  box-shadow: 0px 6px 10px rgba(62, 62, 62, 0.15);
+  background-color: ${styleParams.bg};
+  border-radius: 100px;
+  visibility: ${({ show }) => (show ? "visible" : "hidden")};
+  opacity: ${({ show }) => (show ? "1" : "0")};
+  transition: 0.3s all;
+`;
+
 function Home() {
   const history = useHistory();
   const { mainPadding } = useRWDStyleParams();
   const { screen } = useRWD();
   const isSmScreen = useMemo(() => screen <= screenEnum.sm, [screen]);
+  const [showToTopBtn, setShowToTopBtn] = useState();
+  const refEle = useRef();
+
+  useEffect(() => {
+    const scrollCB = () =>
+      setShowToTopBtn(
+        refEle.current.clientHeight / 2 < refEle.current.scrollTop
+      );
+    refEle.current.addEventListener("scroll", scrollCB);
+  }, []);
 
   return (
-    <Container>
-      <FullHeightContent
-        isSmScreen={isSmScreen}
-        justify="center"
-        flex
-        paddingLeft={mainPadding}
-      >
-        <Title isSmScreen={isSmScreen}>一起享受單車的美好</Title>
-        <FlexBox>
-          你知道你家最近的單車道在哪嗎？
-          <br /> 路易騎收集了全台灣超過 300 條腳踏車道，
-          <br /> 以及 300 個 Youbike 站點，透過簡單的規劃，
-          <br /> 就能找到最適合你的路線，讓我們一起輕鬆上路。
-        </FlexBox>
-        <StyledButton
+    <ScrolledContainer ref={refEle}>
+      <Container>
+        <FullHeightContent
           isSmScreen={isSmScreen}
-          paddingRight={mainPadding}
-          type="primary"
-          onClick={() => history.push("/plan")}
+          justify="center"
+          flex
+          paddingLeft={mainPadding}
         >
-          規劃路線
-        </StyledButton>
-        <ScrollHint />
-      </FullHeightContent>
-      <MainInfos />
-      <MoreInfoBanner />
+          <Title isSmScreen={isSmScreen}>一起享受單車的美好</Title>
+          <FlexBox>
+            你知道你家最近的單車道在哪嗎？
+            <br /> 路易騎收集了全台灣超過 300 條腳踏車道，
+            <br /> 以及 300 個 Youbike 站點，透過簡單的規劃，
+            <br /> 就能找到最適合你的路線，讓我們一起輕鬆上路。
+          </FlexBox>
+          <StyledButton
+            isSmScreen={isSmScreen}
+            paddingRight={mainPadding}
+            type="primary"
+            onClick={() => history.push("/plan")}
+          >
+            規劃路線
+          </StyledButton>
+          <ScrollHint />
+        </FullHeightContent>
+        <MainInfos />
+        <MoreInfoBanner />
 
-      <Footer />
-    </Container>
+        <Footer />
+        <ToTopBtn
+          show={showToTopBtn}
+          right={isSmScreen ? "24" : "70"}
+          onClick={() => {
+            refEle.current.scrollTo({ top: 0, behavior: "smooth" });
+            setShowToTopBtn(false);
+          }}
+        >
+          <ToTop />
+        </ToTopBtn>
+      </Container>
+    </ScrolledContainer>
   );
 }
 
